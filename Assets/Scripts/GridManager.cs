23ee285e4fb4d0b02bf7e9dd42fb256928f5b64d
Zehra Demir <zehrademir.gm@gmail.com>
2025-05-13 18:30:58 +0300

    Update GridManager.cs

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -3,20 +3,26 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     public GameObject tilePrefab;
-    public Transform tileParent;
-    public int gridSize = 3;
+    public Transform gridParent;
 
-    public void CreateGrid(Color[] colorSet)
+    public void GenerateGrid(LevelData levelData)
     {
-        for (int x = 0; x < gridSize; x++)
+        foreach (Transform child in gridParent)
         {
-            for (int y = 0; y < gridSize; y++)
-            {
-                GameObject tileObj = Instantiate(tilePrefab, tileParent);
-                Tile tile = tileObj.GetComponent<Tile>();
-                tile.Initialize(new Vector2Int(x, y), colorSet);
-                GameManager.Instance.tiles[x, y] = tile;
-            }
+            Destroy(child.gameObject);
         }
-    }
-}
+
+        GameManager.Instance.tiles = new Tile[levelData.gridSize, levelData.gridSize];
+
+        for (int x = 0; x < levelData.gridSize; x++)
+        {
+            for (int y = 0; y < levelData.gridSize; y++)
+            {
+                GameObject tileObj = Instantiate(tilePrefab, gridParent);
+                Tile tile = tileObj.GetComponent<Tile>();
+
+                Vector2Int pos = new Vector2Int(x, y);
+                int colorIndex = levelData.startingColors[x + y * levelData.gridSize];
+
+                tile.Initialize(levelData.availableColors, colorIndex, pos);
+                tileObj.GetComponent<Button>().
